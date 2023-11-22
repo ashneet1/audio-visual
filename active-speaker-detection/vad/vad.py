a0b02b1141f822bd06d8fbc8c9ddeb95ad873f3f
Ashneet <ashneetnagi21@gmail.com>
2023-11-21 19:48:40 -0700

    Update vad.py

--- active-speaker-detection/vad/vad.py
+++ active-speaker-detection/vad/vad.py
@@ -1,7 +1,9 @@
+#Modified vad.py
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # For more details about silero-vad see https://github.com/snakers4/silero-vad
-# Usage: python3 vad.py 
+# Usage: python3 vad.py
 
 
 import torch
@@ -14,10 +16,12 @@ model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
                               model='silero_vad',
                               force_reload=False)
 
+#utils has 5 variables now (before it was 7)
+#get_speech_ts_advanced was deleted
+print("utils:",utils)
 (get_speech_ts,
- get_speech_ts_adaptive,
  _, read_audio,
- _, _, _) = utils
+ _, _) = utils
 
 MAX_NUMBER_OF_FILES = 10000
 for n in range(MAX_NUMBER_OF_FILES):
@@ -25,7 +29,9 @@ for n in range(MAX_NUMBER_OF_FILES):
         continue
     wav = read_audio('audios/' + str(n) + '.wav')
     print(wav.shape[0])
-    speech_timestamps = get_speech_ts_adaptive(wav, model, step=100, min_speech_samples=1000, min_silence_samples=1000)
+    #was get_speech_ts_advanced
+    #deleted some arguments as they aren't supported by get_speech_ts
+    speech_timestamps = get_speech_ts(wav, model)
     mask = np.zeros((wav.shape[0],), dtype=int)
     for k in range(len(speech_timestamps)):
         start = speech_timestamps[k]['start']
@@ -33,6 +39,6 @@ for n in range(MAX_NUMBER_OF_FILES):
         print(start, end)
         mask[start:end] = 1
     end
-    
+
     np.savetxt('vads/' + str(n) + '.txt', mask, fmt='%d')
     
