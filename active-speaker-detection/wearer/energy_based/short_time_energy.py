cf94b64ca1fdc6490c1f2112ef2bab1bfb648f0e
Ashneet <ashneetnagi21@gmail.com>
2023-11-21 19:48:35 -0700

    Update short_time_energy.py

--- active-speaker-detection/wearer/energy_based/short_time_energy.py
+++ active-speaker-detection/wearer/energy_based/short_time_energy.py
@@ -1,3 +1,5 @@
+#Modified short_time_energy.py
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Usage: python3 short_time_energy.py directory_of_ego4d_videos data_set
@@ -24,36 +26,41 @@ video_nums = video_nums.astype('int').tolist()
 
 for video_id in video_nums:
      vname = video_dir + '/' + video_files[video_id]
-     print(vname)
-     cap = cv.VideoCapture(vname)
-     video_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-     video_fps =  cap.get(cv.CAP_PROP_FPS)
-     cap.release()
-     
-     sound = audiosegment.from_file(vname)
-     sound = sound.resample(sample_rate_Hz=16000, sample_width=2, channels=1)
-     samples = sound.get_array_of_samples()
-     fps = 16000
-     aud = np.array(samples).astype('float32')/65536.0
-     #total_time = int(aud.shape[0]/fps)
-     
-     eng = []
-     for frame_num in range(video_frames):
-         afirst = int(frame_num * 1.0 / float(video_fps) * fps - 0.5*fps)
-         alast = int(afirst + fps)
-     
-         if afirst < 0:
-            afirst = 0
-        
-         if alast >= aud.shape[0]:
-            alast = aud.shape[0]-1 
-         
-         y = aud[afirst:alast]
-     
-         short_time_eng = np.mean(np.abs(y))
-         eng.append(short_time_eng) 
-        
-     np.savetxt('energy/' + str(video_id) + '.txt', eng, fmt='%f') 
+     #If the video is not in the list of downloaded video clips
+     if not os.path.isfile(vname):
+        print("The video_id does not exists:")
+        print(vname)
+     else:
+      print(vname)
+      cap = cv.VideoCapture(vname)
+      video_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+      video_fps =  cap.get(cv.CAP_PROP_FPS)
+      cap.release()
+
+      sound = audiosegment.from_file(vname)
+      sound = sound.resample(sample_rate_Hz=16000, sample_width=2, channels=1)
+      samples = sound.get_array_of_samples()
+      fps = 16000
+      aud = np.array(samples).astype('float32')/65536.0
+      #total_time = int(aud.shape[0]/fps)
+
+      eng = []
+      for frame_num in range(video_frames):
+          afirst = int(frame_num * 1.0 / float(video_fps) * fps - 0.5*fps)
+          alast = int(afirst + fps)
+
+          if afirst < 0:
+              afirst = 0
+
+          if alast >= aud.shape[0]:
+              alast = aud.shape[0]-1
+
+          y = aud[afirst:alast]
+
+          short_time_eng = np.mean(np.abs(y))
+          eng.append(short_time_eng)
+
+      np.savetxt('energy/' + str(video_id) + '.txt', eng, fmt='%f')
 
 
 
