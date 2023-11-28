99c3b871c97f39e965793ba83e8dc2fadccac521
Ashneet <ashneetnagi21@gmail.com>
2023-11-27 23:28:15 -0500

    Update match_audio.py

--- active-speaker-detection/surrounding_people_audio_matching/mrc/match_audio.py
+++ active-speaker-detection/surrounding_people_audio_matching/mrc/match_audio.py
@@ -25,50 +25,50 @@ for fileno in video_nums:
         print("The file does not exist")
         print(vname)
     else:
-    print(fileno, end = '\r')
-    res = np.loadtxt(asd_results_dir + '/' + str(fileno) + '.txt')
-    if len(res.shape) == 1:
-        res = np.expand_dims(res, axis=0)
-    
-    box = {};
-    
-    for n in range(res.shape[0]):
-        if not (int(res[n][1]) in box):
-           box[int(res[n][1])] = {}
-        box[int(res[n][1])][int(res[n][0])] = [int(res[n][2]), int(res[n][3]), int(res[n][4]), int(res[n][5]), int(res[n][6]), res[n][7]];
-    
-    with open('../../audio_embedding/make_audio_embeddings/embeddings/' + str(fileno) + '.pickle', 'rb') as f:
-            audio_feature = pickle.load(f)
-    
-    afeat = {}
-    for k in box:
-        afeat[k] = []
-        for frame_num in box[k]:
-            p = box[k][frame_num]
-            if p[5] > 0.99 and frame_num < len(audio_feature): 
-                  afeat[k].append(audio_feature[frame_num])
-    
-    traj_len = []
-    for k in afeat:
-           traj_len.append([k, len(box[k])])
+        print(fileno, end = '\r')
+        res = np.loadtxt(asd_results_dir + '/' + str(fileno) + '.txt')
+        if len(res.shape) == 1:
+            res = np.expand_dims(res, axis=0)
         
-    np.savetxt('../results/t' + str(fileno) + '.txt', traj_len, fmt='%d')
-   
-    dists = []    
-    for frame_num in range(0,min(9000,len(audio_feature))):
-        dist = []
-        ss = []
-        q = audio_feature[frame_num]
-        q = np.expand_dims(np.array(q), 0)
+        box = {};
+        
+        for n in range(res.shape[0]):
+            if not (int(res[n][1]) in box):
+            box[int(res[n][1])] = {}
+            box[int(res[n][1])][int(res[n][0])] = [int(res[n][2]), int(res[n][3]), int(res[n][4]), int(res[n][5]), int(res[n][6]), res[n][7]];
+        
+        with open('../../audio_embedding/make_audio_embeddings/embeddings/' + str(fileno) + '.pickle', 'rb') as f:
+                audio_feature = pickle.load(f)
+        
+        afeat = {}
+        for k in box:
+            afeat[k] = []
+            for frame_num in box[k]:
+                p = box[k][frame_num]
+                if p[5] > 0.99 and frame_num < len(audio_feature): 
+                    afeat[k].append(audio_feature[frame_num])
+        
+        traj_len = []
         for k in afeat:
-            if len(afeat[k]) > 0:
-               f = np.array(afeat[k])
-               d = cdist(q, f)
-               d = np.min(d)
-               dist.append(d)
-            else:
-               dist.append(1000) 
-        dists.append(dist)
+            traj_len.append([k, len(box[k])])
+            
+        np.savetxt('../results/t' + str(fileno) + '.txt', traj_len, fmt='%d')
+    
+        dists = []    
+        for frame_num in range(0,min(9000,len(audio_feature))):
+            dist = []
+            ss = []
+            q = audio_feature[frame_num]
+            q = np.expand_dims(np.array(q), 0)
+            for k in afeat:
+                if len(afeat[k]) > 0:
+                f = np.array(afeat[k])
+                d = cdist(q, f)
+                d = np.min(d)
+                dist.append(d)
+                else:
+                dist.append(1000) 
+            dists.append(dist)
    
     np.savetxt('../results/' + str(fileno) + '.txt', dists, fmt='%f')
    
